a6e265412a0046d43e2056edd722c7bd4de919d2
Matin de Wet <matindewet116@gmail.com>
2025-08-10 09:01:59 +0200

    implement domain support

---
+++ Src/Common/Domain.Support/Contracts/IEntity.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain.Support.Contracts;
+
+public interface IEntity<T> : IEntity
+{
+    T Id { get; }
+}
+
+public interface IEntity
+{
+    DateTimeOffset DateCreated { get; }
+}
---
+++ Src/Common/Domain.Support/Implementation/Entity.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Support.Contracts;
+
+namespace Domain.Support.Implementation;
+public abstract class Entity<T> : Entity, IEntity<T>
+{
+    public T Id { get; protected set; } = default!;
+}
+
+public abstract class Entity : IEntity
+{
+    public DateTimeOffset DateCreated { get; private set; } = DateTimeOffset.UtcNow;
+}
